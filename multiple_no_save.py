31a79197dbe7ef4e311315fb438fea25072997ee
Hyeji0828 <janghyeji0828@gmail.com>
2024-01-24 18:12:29 +0900

    apply blur to all faces except the input face

--- multiple_no_save.py
+++ multiple_no_save.py
@@ -2,30 +2,48 @@ from deepface import DeepFace
 from retinaface import RetinaFace
 import cv2
 import os
+import numpy as np
+import utils
 
 save_dir = "./db"
-src_name = "img2.jpg"
-multi_name = "multi3.jpg"
+input_fath = "./db/img2.jpg"
+file_path = "./db/multi5.jpg"
 
 margin = 50
 
-faces = RetinaFace.detect_faces(multi_name)
-img = cv2.imread(multi_name)
+faces = RetinaFace.detect_faces(file_path)
+img = cv2.imread(file_path)
 img_marked = img.copy()
 
-obj = DeepFace.verify(src_name, img, model_name = 'VGG-Face')
-print(obj)
-x = obj['facial_areas']['img2']['x']
-y = obj['facial_areas']['img2']['y']
-w = obj['facial_areas']['img2']['w']
-h = obj['facial_areas']['img2']['h']
-img_marked = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
-cv2.imshow("result", img_marked)
-cv2.waitKey(0)
+verify_box = None
+try: 
+    obj = DeepFace.verify(input_fath, img, model_name = 'VGG-Face')
+    print(obj)
+    x = obj['facial_areas']['img2']['x']
+    y = obj['facial_areas']['img2']['y']
+    w = obj['facial_areas']['img2']['w']
+    h = obj['facial_areas']['img2']['h']
+    # img_marked = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    verify_box = [x,y, x+w, y+h]
+
+except:
+    print("No Face Verified")
+    verify_box = None
 
 for face in faces.values():
     x1, y1, x2, y2 = face['facial_area']
-    img_marked = cv2.rectangle(img_marked, (x1,y1), (x2,y2), (255, 0, 0), 3)
+    
+    if verify_box == None:
+        continue
 
+    iou = utils.IoU(face['facial_area'], verify_box)
+
+    if iou >= 0.5:
+        img_marked = cv2.rectangle(img_marked, (x1,y1), (x2,y2), (0, 255, 0), 3)
+    else:
+        # img_marked = cv2.rectangle(img_marked, (x1,y1), (x2,y2), (255, 0, 0), 3)
+        img_marked[y1:y2, x1:x2] = cv2.blur(img_marked[y1:y2, x1:x2], (30, 30))
+
+    
 cv2.imshow("result", img_marked)
 cv2.waitKey(0)
